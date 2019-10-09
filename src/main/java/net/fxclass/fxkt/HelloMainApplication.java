9852ce8a02e881b22ff639f1ef3c1c5036f285d8
melodyshu <1321497435@qq.com>
2019-10-09 16:57:03 +0800

    第一个springboot2项目

---
+++ src/main/java/net/fxclass/fxkt/HelloMainApplication.java
@@ -0,0 +1,12 @@
+package net.fxclass.fxkt;
+
+import net.fxclass.fxkt.controller.HelloController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMainApplication.class,args);
+    }
+}
